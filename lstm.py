8404e7c9a846252d39acb28b1cda7c6ce3e0c1ed
Sameeha Afrulbasha <sameehaafr@gmail.com>
2023-06-02 17:52:37 -0400

    testing start date and end date

--- lstm.py
+++ lstm.py
@@ -160,8 +160,10 @@ def create_lstm(nsteps, nfeatures, units, activation, dropout):
 #PREDICTION ----------------------------------------------------------------------------------------------------------------------
 st.markdown('## Make Predictions')
 st.caption('The input range represents the range of dates you want to make predictions for. The model will use the data from the previous 10 days to make predictions for the next day.')
-start = st.number_input('Insert a start value for the range', format='%i', min_value=0, value=0)
-stop = st.number_input('Insert a stop value for the range', format='%i', min_value=1, value=8)
+start = st.date_input('Select a start date for the range', value=pd.to_datetime('2020-01-01'))
+stop = st.date_input('Select a stop date for the range', value=pd.to_datetime('2020-01-10'))
+# start = st.number_input('Insert a start value for the range', format='%i', min_value=0, value=0)
+# stop = st.number_input('Insert a stop value for the range', format='%i', min_value=1, value=8)
 combined = make_prediction(start,stop)
 combined['date'] = pd.to_datetime(combined['date']).dt.date
 combined.index = combined['date']
