2fdd7397581a6d9ead5c2eaf440c3b88fa0428d1
Sameeha Afrulbasha <sameehaafr@gmail.com>
2023-07-15 00:08:58 -0400

    chaneg to make pred

--- lstm.py
+++ lstm.py
@@ -98,8 +98,8 @@ def make_prediction(start, stop):
     merged = merge_data()
     merged[DATE] = pd.to_datetime(merged[DATE])
     # Find the nearest available dates before the chosen start and stop dates
-    start_date = merged[DATE][merged[DATE] <= pd.Timestamp(start)].max()
-    stop_date = merged[DATE][merged[DATE] <= pd.Timestamp(stop)].max()
+    start_date = merged.loc[merged[DATE] <= pd.Timestamp(start), DATE].max()
+    stop_date = merged.loc[merged[DATE] <= pd.Timestamp(stop), DATE].max()
     # Predict
     yhat = model.predict(merged.loc[start_date:stop_date, DATA_COL].values.reshape(-1, 1), verbose=0)
     # Denormalize
